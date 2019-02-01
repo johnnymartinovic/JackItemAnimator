9cb69485c99a06b88bec261ae079eb68ba144944
ivanmartinovic <ivan.martinovic@undabot.com>
2019-02-01 14:27:51 +0100

    remove unnecessary stuff

--- sample/src/main/java/com/johnnym/jackitemanimator/sample/MainActivity.kt
+++ sample/src/main/java/com/johnnym/jackitemanimator/sample/MainActivity.kt
@@ -2,8 +2,6 @@ package com.johnnym.jackitemanimator.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.johnnym.jackitemanimator.sample.diffutildemo.DiffUtilDemoActivity
-import com.johnnym.jackitemanimator.sample.notifydemo.NotifyDemoActivity
 import com.johnnym.jackitemanimator.sample.travelino.presentation.TravelinoActivity
 import com.johnnym.jackitemanimator.sample.taxilist.presentation.TaxiListActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,45 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         travelinoButton.setOnClickListener {
             startActivity(TravelinoActivity.createIntent(this))
-        }
-
-        insertItemDemoButton.setOnClickListener {
-            startActivity(NotifyDemoActivity.createIntent(
-                    this,
-                    NotifyDemoActivity.NotifyDemoInitData(
-                            NotifyDemoActivity.NotifyDemoInitData.DemoType.INSERT_ITEM)))
-        }
-
-        moveItemDemoButton.setOnClickListener {
-            startActivity(NotifyDemoActivity.createIntent(
-                    this,
-                    NotifyDemoActivity.NotifyDemoInitData(
-                            NotifyDemoActivity.NotifyDemoInitData.DemoType.MOVE_ITEM)))
-        }
-
-        removeItemRangeDemoButton.setOnClickListener {
-            startActivity(NotifyDemoActivity.createIntent(
-                    this,
-                    NotifyDemoActivity.NotifyDemoInitData(
-                            NotifyDemoActivity.NotifyDemoInitData.DemoType.REMOVE_ITEM_RANGE)))
-        }
-
-        changeItemDemoButton.setOnClickListener {
-            startActivity(NotifyDemoActivity.createIntent(
-                    this,
-                    NotifyDemoActivity.NotifyDemoInitData(
-                            NotifyDemoActivity.NotifyDemoInitData.DemoType.CHANGE_ITEM)))
-        }
-
-        changeItemWithPayloadDemoButton.setOnClickListener {
-            startActivity(NotifyDemoActivity.createIntent(
-                    this,
-                    NotifyDemoActivity.NotifyDemoInitData(
-                            NotifyDemoActivity.NotifyDemoInitData.DemoType.CHANGE_ITEM_WITH_PAYLOAD)))
-        }
-
-        diffUtilDemoButton.setOnClickListener {
-            startActivity(DiffUtilDemoActivity.createIntent(this))
         }
     }
 }
